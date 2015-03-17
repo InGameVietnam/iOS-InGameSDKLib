12c1adeb65e946588560e02062c9ff8e54255adb
quoctc <quoctc@gmail.com>
2015-03-17 14:18:54 +0700

    fix example cocos2dx project

--- IngameSDKExampleCocos2dx/proj.ios_mac/ios/AppController.mm
+++ IngameSDKExampleCocos2dx/proj.ios_mac/ios/AppController.mm
@@ -30,6 +30,7 @@
 #import "Supporter.h"
 //import Ingame Mobile SDK
 #import "SDKViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
 
 @implementation AppController
 
@@ -138,6 +139,22 @@ static AppDelegate s_sharedApplication;
      Called when the application is about to terminate.
      See also applicationDidEnterBackground:.
      */
+}
+
+//INGAMGE this code will be implement at client appdelegate
+// In order to process the response you get from interacting with the Facebook login process,
+// you need to override application:openURL:sourceApplication:annotation:
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
+    // Call FBAppCall's handleOpenURL:sourceApplication to handle Facebook app responses
+    BOOL wasHandled = [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
+    
+    // You can add your app-specific url handling code here if needed
+    
+    return wasHandled;
 }
 
 
