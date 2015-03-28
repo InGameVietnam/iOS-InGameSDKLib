c001b6bef02a50a707dd8cef832915de872d952f
quoctc <quoctc@gmail.com>
2015-03-28 12:30:01 +0700

    Fix Payment
    1. Close payment view when finish a payment
    2. Move Itunes Secret Key to SDK config
    3. Fix payment crash when load product not found

--- Lib/include/InGameSDK/SDKConfig.h
+++ Lib/include/InGameSDK/SDKConfig.h
@@ -9,7 +9,11 @@
 #ifndef InGameSDK_SDKConfig_h
 #define InGameSDK_SDKConfig_h
 
+//get from Ingame Developer Support website
 #define APPID   @"911fe9dbd0f94625b4b591301c0f3818"
 #define APPKEY  @"a2de0d67a7aecbcc89293fbed6712b1a"
+
+//for itune inappurchase -  get from itunes
+#define ITC_CONTENT_PROVIDER_SHARED_SECRET  @"cd41028cb7a24f2c979d3c01a27495af"
 
 #endif
